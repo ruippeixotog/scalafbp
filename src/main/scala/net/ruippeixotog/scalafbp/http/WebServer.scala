d35919b7d6d2b4b07b0ac47d35b623275eeac590
Rui Gonçalves <ruippeixotog@gmail.com>
2016-08-02 00:27:21 +0100

    Handle deserialization exceptions and exceptions inside akka-streams

--- src/main/scala/net/ruippeixotog/scalafbp/http/WebServer.scala
+++ src/main/scala/net/ruippeixotog/scalafbp/http/WebServer.scala
@@ -2,13 +2,15 @@ package net.ruippeixotog.scalafbp.http
 
 import java.util.UUID
 
+import scala.util.{ Failure, Success, Try }
+
 import akka.actor.{ ActorSystem, Props }
 import akka.event.slf4j.SLF4JLogging
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.ws.{ Message => WsMessage, TextMessage => WsTextMessage }
 import akka.http.scaladsl.server.Directives._
-import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Flow
+import akka.stream.{ ActorMaterializer, ActorMaterializerSettings, Supervision }
 import com.typesafe.config.ConfigFactory
 import spray.json._
 
@@ -21,8 +23,15 @@ import net.ruippeixotog.scalafbp.ws.{ SubscriptionManagerActor, WsUtils }
 
 object WebServer extends App with RegistrationHttpService with WsUtils with SLF4JLogging {
   implicit val system = ActorSystem()
-  implicit val materializer = ActorMaterializer()
   implicit val ec = system.dispatcher
+
+  val decider: Supervision.Decider = { e =>
+    log.error("Unhandled exception in stream", e)
+    Supervision.Stop
+  }
+
+  implicit val materializer = ActorMaterializer(
+    ActorMaterializerSettings(system).withSupervisionStrategy(decider))
 
   val config = ConfigFactory.load.getConfig("scalafbp")
   val registryConfig = config.getConfig("registry")
@@ -43,12 +52,25 @@ object WebServer extends App with RegistrationHttpService with WsUtils with SLF4
   // actor dealing with the subscription of multiple WebSocket clients
   val wsManagerActor = system.actorOf(Props(new SubscriptionManagerActor(protocolActor)))
 
+  def readWsMessage(wsMsg: WsMessage): List[Message] = wsMsg match {
+    case WsTextMessage.Strict(text) =>
+      Try(text.parseJson.convertTo[Message]) match {
+        case Success(msg) => List(msg)
+        case Failure(_) =>
+          log.warn(s"Received illegal FBP message: $text")
+          Nil
+      }
+    case _ => Nil
+  }
+
+  def writeWsMessage(msg: Message): WsMessage = WsTextMessage(msg.toJson.compactPrint)
+
   // creates a `Flow` of WebScoket messages encompassing incoming and outgoing messages to/from a client
   def fbpRuntimeFlow(id: String): Flow[WsMessage, WsMessage, Any] = {
     Flow[WsMessage]
-      .collect { case WsTextMessage.Strict(text) => text.parseJson.convertTo[Message] }
+      .mapConcat(readWsMessage)
       .via(subscriptionFlow[Message, Message](id, wsManagerActor))
-      .map { msg => WsTextMessage(msg.toJson.compactPrint) }
+      .map(writeWsMessage)
   }
 
   // format: OFF
