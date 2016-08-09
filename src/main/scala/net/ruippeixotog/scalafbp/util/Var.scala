24400d6279d192684f6472d4d7e4771a93c66580
Rui Gonçalves <ruippeixotog@gmail.com>
2016-08-09 16:45:37 +0100

    Allow Vars to be undefined

--- src/main/scala/net/ruippeixotog/scalafbp/util/Var.scala
+++ src/main/scala/net/ruippeixotog/scalafbp/util/Var.scala
@@ -4,53 +4,60 @@ import scala.collection.SeqLike
 import scala.collection.generic.CanBuildFrom
 
 sealed abstract class Var[+A] {
-  def get: A
+  def get: Option[A]
 
-  final def map[B](f: A => B): Var[B] = new DepVar[B, Unit]((), _ => f(get), _ => List(this))
+  final def map[B](f: A => B): Var[B] =
+    new DepVar[B, Unit]((), _ => get.map(f), _ => List(this))
 
-  final def flatMap[B](f: A => Var[B]): Var[B] = new DepVar[B, Var[B]](f(get), _.get, List(this, _))
+  final def flatMap[B](f: A => Var[B]): Var[B] =
+    new DepVar[B, Option[Var[B]]](get.map(f), _.flatMap(_.get), List(this) ++ _)
 
-  final def flatten[B](implicit ev: A <:< Var[B]): Var[B] = new DepVar[B, Var[B]](get, _.get, List(this, _))
+  final def flatten[B](implicit ev: Option[A] <:< Option[Var[B]]): Var[B] =
+    new DepVar[B, Option[Var[B]]](get, _.flatMap(_.get), List(this) ++ _)
 
   final def foreach[U](f: A => U): Unit = {
     dependentActions :+= f
-    f(get)
+    get.foreach(f)
   }
 
-  final def zip[B](v: Var[B]): Var[(A, B)] = new DepVar[(A, B), Unit]((), _ => (get, v.get), _ => List(this, v))
-  final def unzip[A1, A2](implicit ev: A => (A1, A2)): (Var[A1], Var[A2]) = (map(_._1), map(_._2))
+  final def zip[B](v: Var[B]): Var[(A, B)] =
+    new DepVar[(A, B), Unit]((), _ => for (a1 <- get; a2 <- v.get) yield (a1, a2), _ => List(this, v))
+
+  final def unzip[A1, A2](implicit ev: A => (A1, A2)): (Var[A1], Var[A2]) =
+    (map(_._1), map(_._2))
 
   protected[util] var dependentVars: Set[DepVar[_, _]] = Set.empty
   protected[this] var dependentActions: Seq[A => Any] = Vector.empty
 }
 
 object Var {
-  def apply[A](a: A): Var[A] = new Source(a)
-  def source[A](a: A): Source[A] = new Source(a)
+  def undefined[A](): Source[A] = new Source(None)
+  def apply[A](a: A): Source[A] = new Source(Some(a))
+  def constant[A](a: A): Var[A] = new Source(Some(a))
 
   def sequence[A, M[X] <: SeqLike[X, M[X]]](in: M[Var[A]])(
     implicit
     cbf: CanBuildFrom[M[Var[A]], A, M[A]],
     cbf2: CanBuildFrom[M[A], A, M[A]]): Var[M[A]] = {
 
-    in.foldRight(Var(cbf(in).result())) { (v, acc) =>
+    in.foldRight(Var.constant(cbf(in).result())) { (v, acc) =>
       for (builder <- acc; elem <- v) yield elem +: builder
     }
   }
 }
 
-final class Source[A] private[util] (private[this] var value: A) extends Var[A] {
+final class Source[A] private[util] (private[this] var value: Option[A]) extends Var[A] {
   def get = value
 
   def set(a: A): Unit = {
-    value = a
-    dependentActions.foreach(_(value))
+    value = Some(a)
+    dependentActions.foreach(_(a))
     dependentVars.foreach(_.markForUpdate())
     dependentVars.foreach(_.onMarkingFinished())
   }
 }
 
-final class DepVar[A, I] private[util] (genBase: => I, genVal: I => A, genDeps: I => Seq[Var[_]]) extends Var[A] {
+final class DepVar[A, I] private[util] (genBase: => I, genVal: I => Option[A], genDeps: I => Seq[Var[_]]) extends Var[A] {
   private[this] var lastBase = genBase
   private[this] var lastValue = genVal(lastBase)
   private[this] var lastDeps = genDeps(lastBase)
@@ -61,7 +68,7 @@ final class DepVar[A, I] private[util] (genBase: => I, genVal: I => A, genDeps: 
   def get = {
     if (needsUpdate) {
       lastValue = genVal(lastBase)
-      dependentActions.foreach(_(lastValue))
+      lastValue.foreach { value => dependentActions.foreach(_(value)) }
       needsUpdate = false
     }
     lastValue
--- src/test/scala/net/ruippeixotog/scalafbp/util/VarSpec.scala
+++ src/test/scala/net/ruippeixotog/scalafbp/util/VarSpec.scala
@@ -7,157 +7,194 @@ class VarSpec extends Specification {
   "A Var" should {
 
     "have proper constructors" in {
-      Var(2).get mustEqual 2
+      Var.undefined[Int]().get must beNone
+      Var(2).get must beSome(2)
+      Var.constant(2).get must beSome(2)
 
-      val x = Var.source(2)
-      x.get mustEqual 2
+      val x = Var.undefined[Int]()
+      x.get must beNone
       x.set(1)
-      x.get mustEqual 1
+      x.get must beSome(1)
+      x.set(2)
+      x.get must beSome(2)
     }
 
     "have a correct map method" in {
-      val x = Var.source("aaa")
+      val x = Var.undefined[String]()
       val y = x.map(_.length)
-      y.get mustEqual 3
+      y.get must beNone
 
       x.set("abcde")
-      y.get mustEqual 5
+      y.get must beSome(5)
     }
 
     "have a correct flatMap method" in {
-      val x = Var.source(3)
-      val y = Var.source(2)
+      val x = Var.undefined[Int]()
+      val y = Var.undefined[Int]()
 
       val sum = x.flatMap { xv => y.map(_ + xv) }
-      sum.get mustEqual 5
+      sum.get must beNone
+
+      y.set(2)
+      x.get must beNone
+      sum.get must beNone
 
       x.set(10)
-      y.get mustEqual 2
-      sum.get mustEqual 12
+      y.get must beSome(2)
+      sum.get must beSome(12)
 
       y.set(100)
-      x.get mustEqual 10
-      sum.get mustEqual 110
+      x.get must beSome(10)
+      sum.get must beSome(110)
     }
 
     "have a correct flatten method" in {
-      val x = Var.source(3)
-      val y = Var.source(2)
+      val x = Var.undefined[Int]()
+      val y = Var.undefined[Int]()
 
       val xy = x.map { xVal => y.map(_ + xVal) }
       val sum = xy.flatten
-      sum.get mustEqual 5
+      sum.get must beNone
+
+      y.set(2)
+      x.get must beNone
+      sum.get must beNone
 
       x.set(10)
-      y.get mustEqual 2
-      sum.get mustEqual 12
+      y.get must beSome(2)
+      sum.get must beSome(12)
 
       y.set(100)
-      x.get mustEqual 10
-      sum.get mustEqual 110
+      x.get must beSome(10)
+      sum.get must beSome(110)
     }
 
     "have a correct foreach method" in {
-      val x = Var.source("aaa")
+      val x = Var("aaa")
       val y = x.map(_.length)
+      val z = Var.undefined[String]()
 
       var lastValueX = ""
       var lastValueY = 0
+      var lastValueZ = ""
       x.foreach { xVal => lastValueX = xVal }
       y.foreach { yVal => lastValueY = yVal }
+      z.foreach { zVal => lastValueZ = zVal }
 
       lastValueX mustEqual "aaa"
       lastValueY mustEqual 3
+      lastValueZ mustEqual ""
 
       x.set("abcde")
       lastValueX mustEqual "abcde"
       lastValueY mustEqual 5
+
+      z.set("qwerty")
+      lastValueZ mustEqual "qwerty"
     }
 
     "have a correct zip method" in {
-      val x = Var.source(3)
-      val y = Var.source(5)
+      val x = Var.undefined[Int]()
+      val y = Var.undefined[Int]()
       val xy = x.zip(y)
 
-      xy.get mustEqual (3, 5)
+      xy.get must beNone
 
       x.set(1)
-      xy.get mustEqual (1, 5)
+      xy.get must beNone
 
       y.set(10)
-      xy.get mustEqual (1, 10)
+      xy.get must beSome((1, 10))
+
+      x.set(2)
+      xy.get must beSome((2, 10))
+
+      y.set(20)
+      xy.get must beSome((2, 20))
     }
 
     "have a correct unzip method" in {
-      val xy = Var.source((3, 5))
+      val xy = Var.undefined[(Int, Int)]()
       val (x, y) = xy.unzip
 
-      x.get mustEqual 3
-      y.get mustEqual 5
+      x.get must beNone
+      y.get must beNone
 
       xy.set((1, 10))
-      x.get mustEqual 1
-      y.get mustEqual 10
+      x.get must beSome(1)
+      y.get must beSome(10)
+
+      xy.set((2, 20))
+      x.get must beSome(2)
+      y.get must beSome(20)
     }
 
     "update correctly the dynamic dependencies" in {
-      val a = Var.source(3)
-      val b = Var.source(5)
-      val switch = Var.source(false)
+      val a = Var.undefined[Int]()
+      val b = Var.undefined[Int]()
+      val switch = Var(false)
       val res = switch.flatMap { sw => if (sw) a else b }
 
-      res.get mustEqual 5
+      res.get must beNone
 
       var called = false
       res.foreach { _ => called = true }
-      called must beTrue
+      called must beFalse
 
+      a.set(3)
+      called must beFalse
+      res.get must beNone
+
+      b.set(5)
+      called must beTrue
+      res.get must beSome(5)
       called = false
+
       a.set(2)
       called must beFalse
-      res.get mustEqual 5
+      res.get must beSome(5)
 
       b.set(7)
       called must beTrue
-      res.get mustEqual 7
-
+      res.get must beSome(7)
       called = false
+
       switch.set(true)
       called must beTrue
-      res.get mustEqual 2
-
+      res.get must beSome(2)
       called = false
+
       a.set(1)
       called must beTrue
-      res.get mustEqual 1
-
+      res.get must beSome(1)
       called = false
+
       b.set(10)
       called must beFalse
-      res.get mustEqual 1
+      res.get must beSome(1)
     }
 
     "handle correctly doubly dependent variables" in {
-      val a = Var.source(3)
-      val b = Var.source(10)
+      val a = Var(3)
+      val b = Var(10)
       val c = a.flatMap { aVal => if (aVal < 5) a else b }
 
-      c.get mustEqual 3
+      c.get must beSome(3)
       a.set(4)
-      c.get mustEqual 4
+      c.get must beSome(4)
 
       a.set(8)
-      c.get mustEqual 10
+      c.get must beSome(10)
 
       a.set(1)
-      c.get mustEqual 1
+      c.get must beSome(1)
     }
 
     "handle correctly a complex dependency graph" in {
-      val a = Var.source("abcba")
+      val a = Var("abcba")
       val b = a.map(_.length)
       val c = a.map(_.distinct.length)
-      val d = Var.source(1)
+      val d = Var.undefined[Int]()
       val e = for { bVal <- b; cVal <- c; dVal <- d } yield bVal + cVal + dVal
 
       var called = List.empty[(Char, Any)]
@@ -167,14 +204,17 @@ class VarSpec extends Specification {
       d.foreach { dVal => called +:= ('d' -> dVal) }
       e.foreach { eVal => called +:= ('e' -> eVal) }
 
-      called must containTheSameElementsAs(Seq('a' -> "abcba", 'b' -> 5, 'c' -> 3, 'd' -> 1, 'e' -> 9))
-
+      called must containTheSameElementsAs(Seq('a' -> "abcba", 'b' -> 5, 'c' -> 3))
       called = Nil
+
+      d.set(1)
+      called must containTheSameElementsAs(Seq('d' -> 1, 'e' -> 9))
+      called = Nil
+
       a.set("ab")
-      c.get mustEqual 2
       called must containTheSameElementsAs(Seq('a' -> "ab", 'b' -> 2, 'c' -> 2, 'e' -> 5))
-
       called = Nil
+
       d.set(2)
       called must containTheSameElementsAs(Seq('d' -> 2, 'e' -> 6))
     }
