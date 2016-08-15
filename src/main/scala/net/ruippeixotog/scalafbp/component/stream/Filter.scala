67fe7c4584669ccac74679c2861941750e099ed2
Rui Gonçalves <ruippeixotog@gmail.com>
2016-08-15 21:48:15 +0100

    Add a Filter component

---
+++ src/main/scala/net/ruippeixotog/scalafbp/component/stream/Filter.scala
@@ -0,0 +1,35 @@
+package net.ruippeixotog.scalafbp.component.stream
+
+import akka.actor.Props
+import spray.json._
+
+import net.ruippeixotog.scalafbp.component.SimpleComponentActor.VarDefinition
+import net.ruippeixotog.scalafbp.component._
+import net.ruippeixotog.scalafbp.util.{ NashornEngine, Var }
+
+case object Filter extends Component {
+  val name = "stream/Filter"
+  val description = "Emits only the elements of a stream obeying a condition"
+  val icon = Some("filter")
+  val isSubgraph = true
+
+  val inPort = InPort[JsValue]("in", "The stream to filter")
+  val funcPort = InPort[String]("func", "A predicate with arguments (acc, x) used to create the next element. " +
+    "While not defined, all elements pass unfiltered.")
+  val inPorts = List(inPort, funcPort)
+
+  val outPort = OutPort[JsValue]("out", "The filtered elements")
+  val outPorts = List(outPort)
+
+  val instanceProps = Props(new SimpleComponentActor(this) with VarDefinition with NashornEngine {
+    val defaultFuncVar = Var.constant[JsFunction]({ _ => JsTrue })
+    val funcVar = funcPort.value.map(JsFunction(_)).orElse(defaultFuncVar)
+
+    funcVar.flatMap { f =>
+      inPort.value.map(f).flatMap {
+        case JsFalse | JsNull => Var.undefined()
+        case _ => inPort.value
+      }
+    }.pipeTo(outPort)
+  })
+}
---
+++ src/test/scala/net/ruippeixotog/scalafbp/component/stream/FilterSpec.scala
@@ -0,0 +1,46 @@
+package net.ruippeixotog.scalafbp.component.stream
+
+import spray.json._
+
+import net.ruippeixotog.scalafbp.component.{ AutoTerminateSpec, ComponentSpec }
+
+class FilterSpec extends ComponentSpec with AutoTerminateSpec {
+  val component = Filter
+
+  "A Filter component" should {
+
+    "let every element pass until the function is defined" in new ComponentInstance {
+      Filter.inPort.send(JsNumber(0))
+      Filter.outPort must receive(JsNumber(0))
+      Filter.inPort.send(JsNumber(3))
+      Filter.outPort must receive(JsNumber(3))
+    }
+
+    "use the filtering function one it is defined" in new ComponentInstance {
+      Filter.inPort.send(JsNumber(0))
+      Filter.outPort must receive(JsNumber(0))
+
+      Filter.funcPort.send("return x > 0")
+      Filter.inPort.send(JsNumber(0))
+      Filter.outPort must receiveNothing
+      Filter.inPort.send(JsNumber(3))
+      Filter.outPort must receive(JsNumber(3))
+    }
+
+    "update the filtering function if it changes" in new ComponentInstance {
+      Filter.funcPort.send("return x > 0")
+      Filter.inPort.send(JsNumber(0))
+      Filter.outPort must receiveNothing
+      Filter.inPort.send(JsNumber(3))
+      Filter.outPort must receive(JsNumber(3))
+
+      Filter.funcPort.send("return x <= 0")
+      Filter.inPort.send(JsNumber(0))
+      Filter.outPort must receive(JsNumber(0))
+      Filter.inPort.send(JsNumber(3))
+      Filter.outPort must receiveNothing
+    }
+
+    terminateItselfWhenAllInPortsAreClosed
+  }
+}
