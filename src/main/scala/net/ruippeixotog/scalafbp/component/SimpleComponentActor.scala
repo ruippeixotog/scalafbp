1c6730668e9b8e8c3d9b2a328f9d94a6e959d144
Rui Gonçalves <ruippeixotog@gmail.com>
2016-08-15 19:46:03 +0100

    Add port freezing helper and rewrite Concat

--- src/main/scala/net/ruippeixotog/scalafbp/component/SimpleComponentActor.scala
+++ src/main/scala/net/ruippeixotog/scalafbp/component/SimpleComponentActor.scala
@@ -1,8 +1,10 @@
 package net.ruippeixotog.scalafbp.component
 
-import akka.actor.Actor
+import scala.collection.immutable.Queue
+
+import akka.actor.{ Actor, ActorRef }
 import akka.contrib.pattern.ReceivePipeline
-import akka.contrib.pattern.ReceivePipeline.Inner
+import akka.contrib.pattern.ReceivePipeline.{ HandledCompletely, Inner }
 
 import net.ruippeixotog.scalafbp.component.ComponentActor._
 import net.ruippeixotog.scalafbp.component.SimpleComponentActor._
@@ -25,13 +27,13 @@ object SimpleComponentActor {
     }
   }
 
-  trait VarDefinition extends Actor {
+  trait VarDefinition extends Actor with ReceivePipeline {
     def component: Component
 
     private[this] val sourcesMap =
       component.inPorts.map(_.id -> Var.undefined[Any]()).toMap
 
-    implicit class RichInPort[A](inPort: InPort[A]) {
+    implicit class VarEnabledInPort[A](inPort: InPort[A]) {
       def value: Var[A] = sourcesMap(inPort.id).asInstanceOf[Var[A]]
     }
 
@@ -42,8 +44,49 @@ object SimpleComponentActor {
       }
     }
 
-    final def receive = {
-      case Incoming(port, data) => sourcesMap(port).set(data)
+    pipelineInner {
+      case msg @ Incoming(port, data) =>
+        sourcesMap(port).set(data)
+        Inner(msg)
+    }
+
+    def receive: Receive = Actor.emptyBehavior
+  }
+
+  trait PortFreezing extends Actor with ReceivePipeline {
+    private[this] var frozenPorts = Map[String, Option[Queue[(Any, ActorRef)]]]()
+
+    implicit class FreezableInPort[A](inPort: InPort[A]) {
+      def isFrozen = frozenPorts.get(inPort.id).exists(_.isDefined)
+
+      def freeze() = frozenPorts += (inPort.id -> Some(Queue.empty))
+
+      def unfreeze() = frozenPorts.get(inPort.id).flatten.foreach { queue =>
+        queue.foreach((self.tell _).tupled)
+        frozenPorts -= inPort.id
+      }
+
+      def isIgnored = frozenPorts.get(inPort.id).contains(None)
+
+      def ignore() = frozenPorts += (inPort.id -> None)
+
+      def unignore() = frozenPorts.get(inPort.id).foreach {
+        case None => frozenPorts -= inPort.id
+        case _ => // nothing to do here
+      }
+    }
+
+    private[this] def handleMessage(msg: Any, port: String) = frozenPorts.get(port) match {
+      case None => Inner(msg)
+      case Some(None) => HandledCompletely
+      case Some(Some(queue)) =>
+        frozenPorts += (port -> Some(queue.enqueue(msg, sender())))
+        HandledCompletely
+    }
+
+    pipelineOuter {
+      case msg @ Incoming(port, _) => handleMessage(msg, port)
+      case msg @ InPortDisconnected(port) => handleMessage(msg, port)
     }
   }
 }
--- src/main/scala/net/ruippeixotog/scalafbp/component/stream/Concat.scala
+++ src/main/scala/net/ruippeixotog/scalafbp/component/stream/Concat.scala
@@ -1,10 +1,11 @@
 package net.ruippeixotog.scalafbp.component.stream
 
-import akka.actor.{ Actor, Props, Stash }
+import akka.actor.Props
 import spray.json.JsValue
 
 import net.ruippeixotog.scalafbp.component.ComponentActor._
-import net.ruippeixotog.scalafbp.component.{ Component, InPort, OutPort }
+import net.ruippeixotog.scalafbp.component.SimpleComponentActor.{ PortFreezing, VarDefinition }
+import net.ruippeixotog.scalafbp.component._
 
 case object Concat extends Component {
   val name = "stream/Concat"
@@ -19,22 +20,13 @@ case object Concat extends Component {
   val outPort = OutPort[JsValue]("out", "The concatenated stream")
   val outPorts = List(outPort)
 
-  val instanceProps = Props(new Actor with Stash {
+  val instanceProps = Props(new SimpleComponentActor(this) with VarDefinition with PortFreezing {
+    in1Port.value.pipeTo(outPort)
+    in2Port.value.pipeTo(outPort)
+    in2Port.freeze()
 
-    def sendingFirst: Receive = {
-      case Incoming("in1", data) => sender() ! Outgoing("out", data)
-      case Incoming("in2", data) => stash()
-      case InPortDisconnected("in1") =>
-        unstashAll()
-        context.become(sendingSecond)
-      case InPortDisconnected("in2") => stash()
+    override def receive = {
+      case InPortDisconnected("in1") => in2Port.unfreeze()
     }
-
-    def sendingSecond: Receive = {
-      case Incoming("in2", data) => sender() ! Outgoing("out", data)
-      case InPortDisconnected("in2") => context.stop(self)
-    }
-
-    def receive = sendingFirst
   })
 }
