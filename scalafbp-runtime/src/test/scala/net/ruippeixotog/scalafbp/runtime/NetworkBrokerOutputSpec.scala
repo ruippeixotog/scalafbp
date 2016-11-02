0747a9fdc02572b8313fc0000adc2c647c9fba36
Rui Gonçalves <ruippeixotog@gmail.com>
2016-11-02 21:45:26 +0000

    Add tests for broker output of messages to the output actor

--- scalafbp-runtime/src/test/scala/net/ruippeixotog/scalafbp/runtime/NetworkBrokerOutputSpec.scala
+++ scalafbp-runtime/src/test/scala/net/ruippeixotog/scalafbp/runtime/NetworkBrokerOutputSpec.scala
@@ -2,34 +2,97 @@ package net.ruippeixotog.scalafbp.runtime
 
 import scala.language.reflectiveCalls
 
+import akka.actor.{ Actor, Props }
+import spray.json.DefaultJsonProtocol._
+import spray.json.JsString
+
+import net.ruippeixotog.scalafbp.component.ComponentActor._
+import net.ruippeixotog.scalafbp.runtime.NetworkBroker.{ Connect, Data, Disconnect, ProcessError }
+
 class NetworkBrokerOutputSpec extends NetworkBrokerSpec {
 
   "A NetworkBroker" should {
 
     "send relevant information to the output actor" in {
 
-      "send Connected, Data and Disconnected messages when an initial value is sent" in {
-        todo
+      "send Connect, Data and Disconnect messages when an initial value is sent" in new BrokerInstance {
+        lazy val graph = new SingleNodeGraph {
+          val n1Probe = probeBehavior(n1)
+          initial("aaa") ~> (n1, 1)
+        }
+
+        outputProbe must receive(Connect(graph.id, None, PortRef(graph.n1, "in1")))
+        outputProbe must receive(Data(graph.id, None, PortRef(graph.n1, "in1"), JsString("aaa")))
+        outputProbe must receive(Disconnect(graph.id, None, PortRef(graph.n1, "in1")))
       }
 
-      "send Connected messages for every edge when the network starts" in {
-        todo
+      "send Connect messages for every edge when the network starts" in new BrokerInstance {
+        lazy val graph = new ThreeNodeGraph {
+          (n1, 1) ~> (n2, 1) ~> (n3, 1)
+        }
+
+        outputProbe must receive.allOf(
+          Connect(graph.id, Some(PortRef(graph.n1, "out1")), PortRef(graph.n2, "in1")),
+          Connect(graph.id, Some(PortRef(graph.n2, "out1")), PortRef(graph.n3, "in1")))
       }
 
-      "send Data messages each time a message is routed" in {
-        todo
+      "send Data messages each time a message is routed" in new BrokerInstance {
+        lazy val graph = new TwoNodeGraph {
+          val (_, n1Proxy) = probeBehaviorWithProxyRef(n1)
+          (n1, 1) ~> (n2, 1)
+        }
+
+        graph.n1Proxy ! Outgoing("out1", "d")
+        outputProbe must receive(
+          Data(graph.id, Some(PortRef(graph.n1, "out1")), PortRef(graph.n2, "in1"), JsString("d"))).afterOthers
+
+        graph.n1Proxy ! Outgoing("out1", "e")
+        outputProbe must receive(
+          Data(graph.id, Some(PortRef(graph.n1, "out1")), PortRef(graph.n2, "in1"), JsString("e")))
       }
 
-      "send Disconnected messages for every closed route" in {
-        todo
+      "send Disconnect messages for every closed route" in new BrokerInstance {
+        lazy val graph = new ThreeNodeGraph {
+          val (_, n2Proxy) = probeBehaviorWithProxyRef(n2)
+          (n1, 1) ~> (n2, 1) ~> (n3, 1)
+        }
+
+        graph.n2Proxy ! DisconnectInPort("in1")
+        outputProbe must receive(
+          Disconnect(graph.id, Some(PortRef(graph.n1, "out1")), PortRef(graph.n2, "in1"))).afterOthers
+
+        graph.n2Proxy ! DisconnectOutPort("out1")
+        outputProbe must receive(
+          Disconnect(graph.id, Some(PortRef(graph.n2, "out1")), PortRef(graph.n3, "in1")))
       }
 
-      "send Output messages when a components emits output" in {
-        todo
+      "send Output messages when a components emits output" in new BrokerInstance {
+        lazy val graph = new SingleNodeGraph {
+          val (_, n1Proxy) = probeBehaviorWithProxyRef(n1)
+        }
+
+        graph.n1Proxy ! Message("some text")
+        outputProbe must receive(Message("some text")).afterOthers
+
+        graph.n1Proxy ! PreviewURL("cute gif", "http://example.com/image.gif")
+        outputProbe must receive(PreviewURL("cute gif", "http://example.com/image.gif"))
       }
 
-      "send ProcessError messages when a component fails" in {
-        todo
+      "send ProcessError messages when a component fails" in new BrokerInstance {
+
+        lazy val instanceProps = Props(new Actor {
+          def receive = {
+            case Incoming("in1", "boom") => throw new Exception("test error")
+            case _ => // ignore
+          }
+        })
+
+        lazy val graph = new SingleNodeGraph {
+          behavior(n1, instanceProps)
+          initial("boom") ~> (n1, 1)
+        }
+
+        outputProbe must receive(ProcessError(graph.id, graph.n1, "test error")).afterOthers
       }
     }
   }
